e3a1be906fbfe3c3bbde34853fa192b64c6a8309
Matthew Johnson <mattj20@gmail.com>
2021-04-08 01:11:30 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,5 +2,5 @@ const chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 let newArray
 
 function addElementToBeginningOfArray(array, element) {
-  return newArray = [element, ]
+  return newArray = [element, array]
 }
